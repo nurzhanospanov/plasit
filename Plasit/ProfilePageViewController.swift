10e5676bc6477799272483a6143accf86c9d67a4
nurzhanospanov <ospanovnurzhan@gmail.com>
2016-07-14 14:20:50 -0700

    added ProfilePageViewController.swift

---
+++ Plasit/ProfilePageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfilePageViewController.swift
+//  Plasit
+//
+//  Created by nurzhan on 7/14/16.
+//  Copyright © 2016 Nurzhan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfilePageViewController: UIViewController {
+
+
+}
