a39df3e5703135951eadc0474a94ee006e1b73e6
nurzhanospanov <ospanovnurzhan@gmail.com>
2016-07-14 09:59:21 -0700

    initial commit

---
+++ Plasit/ListOfCategoriesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfCategoriesViewController.swift
+//  Plasit
+//
+//  Created by nurzhan on 7/14/16.
+//  Copyright © 2016 Nurzhan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ListOfCategoriesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+}
