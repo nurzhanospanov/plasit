f9ee9f3b8291073b032ec1989b7e84abe1ffbe07
nurzhanospanov <ospanovnurzhan@gmail.com>
2016-07-22 15:33:25 -0700

    changed ProfilePageViewController into TableViewController

--- Plasit/ProfilePageViewController.swift
+++ Plasit/ProfilePageViewController.swift
@@ -12,5 +12,19 @@ import UIKit
 class ProfilePageViewController: UITableViewController {
 
     @IBOutlet var profilePageTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        return cell
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 }
