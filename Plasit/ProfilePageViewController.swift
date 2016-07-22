23e2dfc5feda311dd438912ea4a60086b1dd432f
nurzhanospanov <ospanovnurzhan@gmail.com>
2016-07-22 16:22:24 -0700

    added labels and image view for Profile Page View Controller

--- Plasit/ProfilePageViewController.swift
+++ Plasit/ProfilePageViewController.swift
@@ -8,23 +8,44 @@
 
 import Foundation
 import UIKit
+import Parse
 
 class ProfilePageViewController: UITableViewController {
 
+    
     @IBOutlet var profilePageTableView: UITableView!
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var lastNameLabel: UILabel!
+    
+    @IBOutlet weak var profilePictureImageView: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
-        return cell
-        
-    }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
+    
+    
+    
+    
+    
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        
+//        let cell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+//        return cell
+//        
+//    }
+//    
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//    
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 3 
+//    }
 }
