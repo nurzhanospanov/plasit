fdded6a45124858703823bc4717f163151ec1042
nurzhanospanov <ospanovnurzhan@gmail.com>
2016-07-27 14:45:42 -0700

    added two buttons on Place Post View Controller

--- Plasit/PlacePostViewController.swift
+++ Plasit/PlacePostViewController.swift
@@ -31,6 +31,37 @@ class PlacePostViewController: UITableViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     
+    @IBOutlet weak var beenHereButton: UIButton!
+    
+    @IBOutlet weak var wantToGoButton: UIButton!
+    
+    @IBAction func beenHereButtonPressed(sender: AnyObject)
+    {
+        if beenHereButton.imageView!.image == UIImage(named: "locationButton")
+        {
+            beenHereButton.setImage(UIImage(named: "locationButtonPressed"), forState: .Normal)
+        }
+        else
+        {
+            beenHereButton.setImage(UIImage(named: "locationButton"), forState: .Normal)
+        }
+    }
+    
+    @IBAction func wantToGoButtonPressed(sender: AnyObject) {
+       
+        if wantToGoButton.imageView!.image == UIImage(named: "airplaneButton")
+        {
+            wantToGoButton.setImage(UIImage(named: "airplaneButtonPressed"), forState: .Normal)
+        }
+        else
+        {
+            wantToGoButton.setImage(UIImage(named: "airplaneButton"), forState: .Normal)
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
@@ -43,8 +74,13 @@ class PlacePostViewController: UITableViewController {
         self.descriptionTextView.text = displayPlace?.placeDescription
         self.titleLabel.text = displayPlace?.placeTitle
     }
-    
-    
+
+
+    func buttonClicked(sender:UIButton)
+        {
+            sender.selected = !sender.selected;
+    }
+
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         print("prepareForSegue: \(segue.identifier)")
